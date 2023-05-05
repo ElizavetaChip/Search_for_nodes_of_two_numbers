fdbaff33fe6e2675d7cec32d0888a9ec9cb8abb1
ElizavetaChip <cvetolika@yandex.ru>
2023-05-05 22:06:44 +0300

    Добавление проектов для тестов

---
+++ test_greatest_common_divisor/test_greatest_common_divisor.cpp
@@ -0,0 +1,17 @@
+#include "pch.h"
+#include "CppUnitTest.h"
+#include "..//Search_for_nodes_of_two_numbers/Search_for_nodes_of_two_numbers.h"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace testgreatestcommondivisor
+{
+	TEST_CLASS(testgreatestcommondivisor)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+		}
+	};
+}
---
+++ test_numerator_denominator/test_numerator_denominator.cpp
@@ -0,0 +1,17 @@
+#include "pch.h"
+#include "CppUnitTest.h"
+#include "..//Search_for_nodes_of_two_numbers/Search_for_nodes_of_two_numbers.h"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace testnumeratordenominator
+{
+	TEST_CLASS(testnumeratordenominator)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+		}
+	};
+}
