1f2b5a068f0e9b9044ca5df8237589aab94371ac
ElizavetaChip <cvetolika@yandex.ru>
2023-05-11 14:32:04 +0300

    Написание теста для функции greatest_common_divisor

--- Search_for_nodes_of_two_numbers/Search_for_nodes_of_two_numbers.cpp
+++ Search_for_nodes_of_two_numbers/Search_for_nodes_of_two_numbers.cpp
@@ -2,8 +2,27 @@
 
 #include <iostream>
 
+int roman_to_int(const char* s);
+unsigned int greatest_common_divisor(unsigned int a, unsigned int b);
+int int_part_of_fraction(int numerator, int denominator);
+
+
 int main()
 {
     std::cout << "Hello World!\n";
 }
 
+int roman_to_int(const char* s)
+{
+    return 0;
+}
+
+unsigned int greatest_common_divisor(unsigned int a, unsigned int b)
+{
+    return 0;
+}
+
+int int_part_of_fraction(int numerator, int denominator)
+{
+    return 0;
+}
--- test_greatest_common_divisor/test_greatest_common_divisor.cpp
+++ test_greatest_common_divisor/test_greatest_common_divisor.cpp
@@ -4,14 +4,95 @@
 
 using namespace Microsoft::VisualStudio::CppUnitTestFramework;
 
-namespace testgreatestcommondivisor
+namespace Testgreatestcommondivisor
 {
-	TEST_CLASS(testgreatestcommondivisor)
+	TEST_CLASS(Testgreatestcommondivisor)
 	{
 	public:
-		
-		TEST_METHOD(TestMethod1)
+
+		TEST_METHOD(SimpleTest)
 		{
+			unsigned int number1 = 6;
+			unsigned int number2 = 9;
+
+			int exp_result = 3;
+
+			int result = greatest_common_divisor(number1, number2);
+
+			Assert::AreEqual(exp_result, result);
 		}
+
+		TEST_METHOD(IdenticalNumbers)
+		{
+			unsigned int number1 = 6;
+			unsigned int number2 = 6;
+
+			int exp_result = 6;
+
+			int result = greatest_common_divisor(number1, number2);
+
+			Assert::AreEqual(exp_result, result);
+		}
+
+		TEST_METHOD(BothNumbersSimple)
+		{
+			unsigned int number1 = 5;
+			unsigned int number2 = 7;
+
+			int exp_result = 1;
+
+			int result = greatest_common_divisor(number1, number2);
+
+			Assert::AreEqual(exp_result, result);
+		}
+
+		TEST_METHOD(OneOfNumbersSimple)
+		{
+			unsigned int number1 = 6;
+			unsigned int number2 = 7;
+
+			int exp_result = 1;
+
+			int result = greatest_common_divisor(number1, number2);
+
+			Assert::AreEqual(exp_result, result);
+		}
+
+		TEST_METHOD(OneOfNumbersMinimal)
+		{
+			unsigned int number1 = 6;
+			unsigned int number2 = 1;
+
+			int exp_result = 1;
+
+			int result = greatest_common_divisor(number1, number2);
+
+			Assert::AreEqual(exp_result, result);
+		}
+
+		TEST_METHOD(OneNumberIsDividedByAnother)
+		{
+			unsigned int number1 = 6;
+			unsigned int number2 = 3;
+
+			int exp_result = 3;
+
+			int result = greatest_common_divisor(number1, number2);
+
+			Assert::AreEqual(exp_result, result);
+		}
+
+		TEST_METHOD(OneOfNumbersIs0)
+		{
+			unsigned int number1 = 0;
+			unsigned int number2 = 4;
+
+			int exp_result = 0;
+
+			int result = greatest_common_divisor(number1, number2);
+
+			Assert::AreEqual(exp_result, result);
+		}
+
 	};
 }
