36e25f03a55bb792d8ac7605d50a9cceecbe56b5
ElizavetaChip <cvetolika@yandex.ru>
2023-05-18 19:27:14 +0300

    Создание теста для функции, переводящей числа из римской системы счисления в арабскую (roman_to_int)

--- test_roman_to_int/test_roman_to_int.cpp
+++ test_roman_to_int/test_roman_to_int.cpp
@@ -4,14 +4,99 @@
 
 using namespace Microsoft::VisualStudio::CppUnitTestFramework;
 
-namespace testromantoint
+namespace Testromantoint
 {
-	TEST_CLASS(testromantoint)
+	TEST_CLASS(Testromantoint)
 	{
 	public:
-		
-		TEST_METHOD(TestMethod1)
+		TEST_METHOD(SimpleTest)
 		{
+			char* roman_number = "V";
+
+			int exp_result = 5;
+
+			int result = roman_to_int(roman_number);
+
+			Assert::AreEqual(exp_result, result);
 		}
+
+		TEST_METHOD(MinimumValueOfTheNumber)
+		{
+			char* roman_number = "I";
+
+			int exp_result = 1;
+
+			int result = roman_to_int(roman_number);
+
+			Assert::AreEqual(exp_result, result);
+		}
+
+		TEST_METHOD(MaximumValueOfTheNumber)
+		{
+			char* roman_number = "MMM";
+
+			int exp_result = 3000;
+
+			int result = roman_to_int(roman_number);
+
+			Assert::AreEqual(exp_result, result);
+		}
+
+		TEST_METHOD(IdenticalLetters)
+		{
+			char* roman_number = "II";
+
+			int exp_result = 2;
+
+			int result = roman_to_int(roman_number);
+
+			Assert::AreEqual(exp_result, result);
+		}
+
+		TEST_METHOD(NextDigitIsBigger)
+		{
+			char* roman_number = "IV";
+
+			int exp_result = 4;
+
+			int result = roman_to_int(roman_number);
+
+			Assert::AreEqual(exp_result, result);
+		}
+
+		TEST_METHOD(NextDigitIsSmaller)
+		{
+			char* roman_number = "VI";
+
+			int exp_result = 6;
+
+			int result = roman_to_int(roman_number);
+
+			Assert::AreEqual(exp_result, result);
+		}
+
+		TEST_METHOD(MaximumNumberLength)
+		{
+			char* roman_number = "MMDCCCLXXXIX";
+
+			int exp_result = 2889;
+
+			int result = roman_to_int(roman_number);
+
+			Assert::AreEqual(exp_result, result);
+		}
+
+		TEST_METHOD(InvalidData)
+		{
+			char* roman_number = "S";
+
+			int exp_result = -1;
+
+			int result = roman_to_int(roman_number);
+
+			Assert::AreEqual(exp_result, result);
+		}
+
 	};
 }
+
