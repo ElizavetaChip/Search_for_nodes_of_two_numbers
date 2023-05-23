f1562259d8c6835224eb7a4e18685188cae05b40
ElizavetaChip <cvetolika@yandex.ru>
2023-05-23 10:19:43 +0300

    Создание теста для деления числителя на знаменатель дл  функции (int_part_of_fraction)

--- test_numerator_denominator/test_numerator_denominator.cpp
+++ test_numerator_denominator/test_numerator_denominator.cpp
@@ -4,14 +4,95 @@
 
 using namespace Microsoft::VisualStudio::CppUnitTestFramework;
 
-namespace testnumeratordenominator
+namespace Testintpartoffraction
 {
-	TEST_CLASS(testnumeratordenominator)
+	TEST_CLASS(Testintpartoffraction)
 	{
 	public:
-		
-		TEST_METHOD(TestMethod1)
+
+		TEST_METHOD(numerator_denominator_areEqual)
 		{
+			unsigned int number1 = 2;
+			unsigned int number2 = 2;
+
+			int exp_result = 1;
+
+			int result = int_part_of_fraction(number1, number2);
+
+			Assert::AreEqual(exp_result, result);
+		}
+
+		TEST_METHOD(numerator_is_zero)
+		{
+			unsigned int number1 = 0;
+			unsigned int number2 = 1;
+
+			int exp_result = 0;
+
+			int result = int_part_of_fraction(number1, number2);
+
+			Assert::AreEqual(exp_result, result);
+		}
+
+		TEST_METHOD(denominator_more_numerator)
+		{
+			unsigned int number1 = 1;
+			unsigned int number2 = 2;
+
+			int exp_result = 0;
+
+			int result = int_part_of_fraction(number1, number2);
+
+			Assert::AreEqual(exp_result, result);
+		}
+
+		TEST_METHOD(Denominator_is_one)
+		{
+			unsigned int number1 = 2;
+			unsigned int number2 = 1;
+
+			int exp_result = 2;
+
+			int result = int_part_of_fraction(number1, number2);
+
+			Assert::AreEqual(exp_result, result);
+		}
+
+		TEST_METHOD(Divided_entirely)
+		{
+			unsigned int number1 = 6;
+			unsigned int number2 = 2;
+
+			int exp_result = 3;
+
+			int result = int_part_of_fraction(number1, number2);
+
+			Assert::AreEqual(exp_result, result);
+		}
+
+		TEST_METHOD(Not_divided_entirely)
+		{
+			unsigned int number1 = 5;
+			unsigned int number2 = 2;
+
+			int exp_result = 2;
+
+			int result = int_part_of_fraction(number1, number2);
+
+			Assert::AreEqual(exp_result, result);
+		}
+
+		TEST_METHOD(Denominator_is_zero)
+		{
+			unsigned int number1 = 2;
+			unsigned int number2 = 0;
+
+			int exp_result = -1;
+
+			int result = int_part_of_fraction(number1, number2);
+
+			Assert::AreEqual(exp_result, result);
 		}
 	};
 }
+
